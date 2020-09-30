2b56f72d21b13ab45faf644f584fbe66427da829
imalwaystired <standnorelation@gmail.com>
2020-09-30 16:52:17 +0900

    lesson06

--- mamazon/views.py
+++ mamazon/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from . models import Product
 
 
@@ -16,6 +16,13 @@ class ProductListView(ListView):
             qs = self.request.GET['query']
             queryset = queryset.filter(name_contains=qs)
         return queryset
+
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'mamazon/detail.html'
+
 
     
 
